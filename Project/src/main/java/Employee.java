805a68e5f6241afc44a8e9d59fd70f11f75ceb6d
ubuntuos <ubuntuos@UbuntuOS.myguest.virtualbox.org>
2022-10-21 17:16:29 +0530

    Second Commit

---
+++ Project/src/main/java/Employee.java
@@ -0,0 +1,23 @@
+
+public class Employee {
+int E_id;
+String E_name;
+public Employee(int e_id, String e_name) {
+	super();
+	E_id = e_id;
+	E_name = e_name;
+}
+
+@Override
+public String toString() {
+	return "Employee [E_id=" + E_id + ", E_name=" + E_name + "]";
+}
+
+public static void main(String [] args)
+{
+Employee employee=new Employee(1, "dinga");
+Employee employee2=new Employee(2, "dinga2");
+System.out.println(employee);
+System.out.println(employee2);
+}
+}
